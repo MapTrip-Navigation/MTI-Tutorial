af23807d115de48eb42b6d239078753e59ccc94e
dirkgermany <dirk@dirk-goldbach.de>
2021-03-26 17:28:37 +0100

    Test files deleted

--- app/src/main/java/de/infoware/mti/mti_tutorial/lessons/Lesson.java
+++ app/src/main/java/de/infoware/mti/mti_tutorial/lessons/Lesson.java
@@ -1,8 +1,6 @@
 package de.infoware.mti.mti_tutorial.lessons;
 
 import androidx.fragment.app.Fragment;
-import de.infoware.android.mti.enums.ApiError;
-import de.infoware.android.mti.enums.Info;
 
 /**
  * This abstract class is the template for all lessons
@@ -22,32 +20,7 @@ public abstract class Lesson {
      * Every lesson should execute some code which can be raised by calling this method
      */
     public abstract void doSomething();
-
-    /**
-     * Can be overwritten by children optionally
-     * @param info Enum with predefined Status informations of MTI
-     * @param i
-     */
-    public void apiInfoMsg (Info info, int i) {
-    }
-
-    /**
-     * Can be overwritten by children optionally
-     * @param requestId Id was delivered before by calling an MTI function
-     * @param apiError Enum with predefined error status of MTI
-     */
-    public void apiInitResult (int requestId, ApiError apiError) {
-    }
-
-    /**
-     * Can be overwritten by children optionally
-     * @param requestId Id was delivered before by calling an MTI function
-     * @param infoText Additional text about error
-     * @param apiError Enum with predefined error status of MTI
-     */
-    public void apiOnError(int requestId, String infoText, ApiError apiError) {
-    }
-
+    
     public String getButtonCaption() {
         return buttonCaption;
     }
